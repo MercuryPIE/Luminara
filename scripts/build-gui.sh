0f7915ce41bcc656aff270bfef43ecbf7db067ed
Thomas Grant <118959686+MercuryPIE@users.noreply.github.com>
2025-09-30 16:10:53 +1000

    Update build-gui.sh

--- scripts/build-gui.sh
+++ scripts/build-gui.sh
@@ -10,6 +10,18 @@ mvn clean install
 # Build the JLink image.
 mvn -pl GUI javafx:jlink
 
+OS_NAME=$(uname -s)
+
+case "$OS_NAME" in
+  Linux*)   OS=linux; ICON="GUI/src/main/resources/Icons/Luminara-Main-Icon.png";;
+  Darwin*)  OS=macos; ICON="GUI/src/main/resources/Icons/Luminara-Main-Icon.png";;
+  MINGW*|MSYS*|CYGWIN*) OS=windows; ICON="GUI/src/main/resources/Icons/Luminara-Main-Icon.ico";;
+  *)  OS=unknown; ICON="";;
+esac
+
+
+
+
 # Package into an app.
 jpackage \
     --name Luminara \
@@ -17,19 +29,10 @@ jpackage \
     --runtime-image GUI/target/LuminaraRuntime \
     --type app-image \
     --module luminara.gui/luminara_gui.RunApp \
-    --icon GUI/src/main/resources/Icons/Luminara-Main-Icon.png \
+    --icon "$ICON" \
     --dest GUI/target/installer
 
 cd ./GUI/target/installer
-
-OS_NAME=$(uname -s)
-
-case "$OS_NAME" in
-  Linux*)   OS=linux;;
-  Darwin*)  OS=macos;;
-  MINGW*|MSYS*|CYGWIN*) OS=windows;;
-  *)  OS=unknown;;
-esac
 
 # Zip/Tar app.
 if [[ "$OS" == "windows" ]]; then
@@ -39,7 +42,6 @@ else
   ARCH_NAME="Luminara-${OS}-${RELEASE_VERSION}.tar.gz"
   tar -czf "$ARCH_NAME" Luminara
 fi
-
 
 echo "build_path=GUI/target/installer/$ARCH_NAME" >> $GITHUB_ENV
 echo "build_name=Luminara-${OS}-${RELEASE_VERSION}" >> $GITHUB_ENV
